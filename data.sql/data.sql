1182b0e7f2cdfb904b0e02410167a872ea12fe3d
anthony577467 <136906838+anthony577467@users.noreply.github.com>
2023-09-30 19:24:12 -0500

    data.sql

--- data.sql/data.sql
+++ data.sql/data.sql
@@ -1,19 +1,19 @@
--- Crear la base de datos data
-DROP DATABASE IF EXISTS data;
-CREATE DATABASE data DEFAULT CHARACTER SET utf8;
--- Poner en uso la base de datos dbGamarraMarket
-USE dbGamarraMarket;
+-- Crear una tabla para almacenar los datos del formulario
+CREATE TABLE FormularioContacto (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Asunto VARCHAR(255) NOT NULL,
+    FechaEnvio DATE NOT NULL,
+    Mensaje TEXT NOT NULL
+);
 
--- Insertar 1 registro en la tabla CLIENTE
-INSERT INTO cliente (tipo_documento, numero_documento, nombres, apellidos, email, celular, fecha_nacimiento, activo)
+-- Insertar datos del formulario en la tabla
+INSERT INTO FormularioContacto (Nombre, Email, Asunto, FechaEnvio, Mensaje)
 VALUES
-('DNI', '12345678', 'Juan', 'Pérez', 'juan.perez@example.com', '987654321', '2000-01-15', '1'),
-('CNE', '87654321', 'María', 'González', 'maria.gonzalez@example.com', '654321987', '1995-05-20', '1'),
-('DNI', '98765432', 'Carlos', 'López', 'carlos.lopez@example.com', '123456789', '1988-11-10', '1');
+    ('Ejemplo 1', 'ejemplo1@example.com', 'Asunto 1', '2023-09-30', 'Mensaje de ejemplo 1'),
+    ('Ejemplo 2', 'ejemplo2@example.com', 'Asunto 2', '2023-09-30', 'Mensaje de ejemplo 2'),
+    ('Ejemplo 3', 'ejemplo3@example.com', 'Asunto 3', '2023-09-30', 'Mensaje de ejemplo 3');
 
--- Verificar los registros insertados
-SELECT * FROM cliente;
-
-
-SELECT * FROM cliente;
-
+-- Verificar que los datos se han insertado correctamente
+SELECT * FROM FormularioContacto;
